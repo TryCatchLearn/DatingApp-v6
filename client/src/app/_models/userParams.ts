d2fee1117ac86155ff5f43df661d452a0eb427ec
trycatchlearn <trycatchlearn@outlook.com>
2021-11-06 09:35:33 +0700

    Pagination fix

--- client/src/app/_models/userParams.ts
+++ client/src/app/_models/userParams.ts
@@ -5,7 +5,7 @@ export class UserParams {
     minAge = 18;
     maxAge = 99;
     pageNumber = 1;
-    pageSize = 5;
+    pageSize = 24;
     orderBy = 'lastActive';
 
     constructor(user: User) {
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -44,8 +44,9 @@ export class MessagesComponent implements OnInit {
   }
 
   pageChanged(event: any) {
-    this.pageNumber = event.page;
-    this.loadMessages();
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
   }
-
 }
